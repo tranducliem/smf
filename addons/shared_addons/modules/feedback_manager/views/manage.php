d1faff8d6e7d7eadcaa266960ffa9f047fe41228
HoangThiTuanDung <tuandung@ubuntu.(none)>
2013-12-27 09:01:50 +0700

    feedback manager v2

--- addons/shared_addons/modules/feedback_manager/views/manage.php
+++ addons/shared_addons/modules/feedback_manager/views/manage.php
@@ -1,56 +1,13 @@
-<a href="<?php echo site_url('feedback_manager/create') ?>" title="<?php echo lang('global:create')?>" class="button"><button class="btn-u">Create Feedback</button></a>
-<br/><br/>
-<table cellspacing="0" border="1px">
-    <thead>
-    <tr>
-        <th>Id</th>
-        <th><?php echo lang('feedback_manager:form_title') ?></th>
-        <th class="collapse"><?php echo lang('feedback_manager:form_description') ?></th>
-        <th><?php echo lang('feedback_manager:form_start_date') ?></th>
-        <th><?php echo lang('feedback_manager:form_end_date') ?></th>
-        <th><?php echo lang('feedback_manager:form_type_id') ?></th>
-        <th><?php echo lang('feedback_manager:form_require') ?></th>
-        <th><?php echo lang('feedback_manager:form_status') ?></th>
-        <th width="180"><?php echo lang('global:actions') ?></th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php echo $this->load->view('admin/partials/filters') ?>
-    <?php foreach ($post as $item) : ?>
-        <tr>
-            <td><?php echo $item->id ?></td>
-            <td><?php echo $item->title ?></td>
-            <td class="collapse"><?php echo $item->description ?></td>
-            <td class="collapse"><?php echo $item->start_date ?></td>
-            <td class="collapse"><?php echo $item->end_date ?></td>
-            <td class="collapse"><?php echo $item->type_id ?></td>
-            <td class="collapse">
-                <button class="btn-u">
-                <?php
-                    if($item->require == 0) echo "Not require";
-                    else echo "Require";
-                ?>
-                </button>
-            </td>
-            <td class="collapse">
-                <button class="btn-u">
-                    <?php
-                        if($item->status == 0) echo "Not start";
-                        else if($item->status == 1) echo "Processing";
-                        else echo "Done";
-                    ?>
-                </button>
-            </td>
-            <td style="padding-top:10px;">
-                <a href="<?php echo site_url('feedback_manager/edit/' . $item->id) ?>" title="<?php echo lang('global:edit')?>" class="button">
-                    <button class="btn-u"><?php echo lang('global:edit')?></button>
-                </a>
-                <a href="<?php echo site_url('feedback_manager/delete/' . $item->id) ?>" title="<?php echo lang('global:delete')?>" class="button confirm">
-                    <button class="btn-u"><?php echo lang('global:delete')?></button></a>
-            </td>
-        </tr>
-    <?php endforeach ?>
-    </tbody>
-</table>
-
-<?php $this->load->view('admin/partials/pagination') ?>
+<div class="span9">
+    <div class="headline"><h3><?php echo "Manage feedback";?></h3></div>
+    <?php if($post){ ?>
+        <?php echo $this->load->view('partials/filters') ?>
+        <?php echo form_open('feedback_manager/action') ?>
+        <div id="filter-result">
+            <?php echo $this->load->view('tables/posts') ?>
+        </div>
+        <?php echo form_close(); ?>
+    <?php }else{ ?>
+        <?php echo lang('feedback_manager:currently_no_posts');?>
+    <?php } ?>
+</div>
---
+++ addons/shared_addons/modules/feedback_manager/views/tables/posts.php
@@ -0,0 +1,60 @@
+<a class="btn-u" href="<?php echo site_url('feedback_manager/create') ?>" title="<?php echo lang('global:create')?>" class="button">Create Feedback</a>
+<br/><br/>
+<table cellspacing="0" border="1px">
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th><?php echo lang('feedback_manager:form_title') ?></th>
+        <th class="collapse"><?php echo lang('feedback_manager:form_description') ?></th>
+        <th><?php echo lang('feedback_manager:form_start_date') ?></th>
+        <th><?php echo lang('feedback_manager:form_end_date') ?></th>
+        <th><?php echo lang('feedback_manager:form_type_id') ?></th>
+        <th><?php echo lang('feedback_manager:form_require') ?></th>
+        <th><?php echo lang('feedback_manager:form_status') ?></th>
+        <th width="180"><?php echo lang('global:actions') ?></th>
+    </tr>
+    </thead>
+    <tbody>
+<!--    --><?php //echo $this->load->view('admin/partials/filters') ?>
+    <?php foreach ($post as $item) : ?>
+        <tr>
+            <td><?php echo $item->id ?></td>
+            <td><?php echo $item->title ?></td>
+            <td class="collapse"><?php echo $item->description ?></td>
+            <td class="collapse"><?php echo $item->start_date ?></td>
+            <td class="collapse"><?php echo $item->end_date ?></td>
+            <td class="collapse"><?php echo $item->type_id ?></td>
+            <td class="collapse">
+                <a href="" class="btn-u">
+                    <?php
+                    if($item->require == 0) echo "Not require";
+                    else echo "Require";
+                    ?>
+                </a>
+            </td>
+            <td class="collapse">
+                <a href="" class="btn-u">
+                    <?php
+                    if($item->status == 0) echo "Not start";
+                    else if($item->status == 1) echo "Processing";
+                    else echo "Done";
+                    ?>
+                </a>
+            </td>
+            <td style="padding-top:10px;">
+                <a class="btn-u" href="<?php echo site_url('feedback_manager/edit/' . $item->id) ?>" title="<?php echo lang('global:edit')?>" class="button">
+                    <?php echo lang('global:edit')?>
+                </a>
+                <a class="btn-u" href="<?php echo site_url('feedback_manager/delete/' . $item->id) ?>" title="<?php echo lang('global:delete')?>" class="button confirm">
+                    <?php echo lang('global:delete')?></a>
+            </td>
+        </tr>
+    <?php endforeach ?>
+    </tbody>
+</table>
+
+<?php if(!empty($pagination['links'])): ?>
+    <div class="paginate">
+        <?php echo $pagination['links'];?>
+    </div>
+<?php endif; ?>
