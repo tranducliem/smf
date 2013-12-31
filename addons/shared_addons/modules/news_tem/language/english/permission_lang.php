deeb851713a62bf7b68384eb898ee472ff238471
duyliempro <duyliempro@gmail.com>
2014-01-01 00:07:22 +0700

    Update News Module V1.0.3

--- addons/shared_addons/modules/news_tem/language/english/permission_lang.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-// news Permissions
-$lang['news:role_put_live']		= 'Put articles live';
-$lang['news:role_edit_live']	= 'Edit live articles';
-$lang['news:role_delete_live'] 	= 'Delete live articles';
