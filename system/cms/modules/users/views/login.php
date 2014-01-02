969f230e2a1c78d5f9c8c8ad5b32e71dadb96567
tranducliem <duyliemapt@gmail.com>
2014-01-02 13:08:26 +0700

    Update Login, Register in User module-liemtd v1.0

--- system/cms/modules/users/views/login.php
+++ system/cms/modules/users/views/login.php
@@ -1,30 +1,40 @@
-<h2 class="page-title" id="page_title"><?php echo lang('user:login_header') ?></h2>
+<?php
+$email = array(
+    'name'        => 'email',
+    'id'          => 'email',
+    'value'       => $this->input->post('email') ? $this->input->post('email') : '',
+    'maxlength'   => '30',
+    'class'       => 'input-xlarge',
+    'placeholder' => lang('global:email'),
+);
+?>
+
+{{ session:messages success="success-box" notice="notice-box" error="error-box" }}
 
 <?php if (validation_errors()): ?>
-<div class="error-box">
-	<?php echo validation_errors();?>
-</div>
+    <div class="error-box">
+        <?php echo validation_errors();?>
+    </div>
 <?php endif ?>
 
-<?php echo form_open('users/login', array('id'=>'login'), array('redirect_to' => $redirect_to)) ?>
-<ul>
-	<li>
-		<label for="email"><?php echo lang('global:email') ?></label>
-		<?php echo form_input('email', $this->input->post('email') ? $this->input->post('email') : '')?>
-	</li>
-	<li>
-		<label for="password"><?php echo lang('global:password') ?></label>
-		<input type="password" id="password" name="password" maxlength="20" />
-	</li>
-	<li id="remember_me">
-		<label><?php echo lang('user:remember') ?></label>
-		<?php echo form_checkbox('remember', '1', false) ?>
-	</li>
-	<li class="form_buttons">
-		<input type="submit" value="<?php echo lang('user:login_btn') ?>" name="btnLogin" /> <span class="register"> | <?php echo anchor('register', lang('user:register_btn'));?></span>
-	</li>
-	<li class="reset_pass">
-		<?php echo anchor('users/reset_pass', lang('user:reset_password_link'));?>
-	</li>
-</ul>
-<?php echo form_close() ?>
+
+<div class="row-fluid">
+    <?php echo form_open('users/login', array('id'=>'login', 'class'=>'log-page'), array('redirect_to' => $redirect_to)) ?>
+    <h3><?php echo lang('user:login_header') ?></h3>
+    <div class="input-prepend">
+        <span class="add-on"><i class="icon-user"></i></span>
+        <?php  echo form_input($email); ?>
+    </div>
+    <div class="input-prepend">
+        <span class="add-on"><i class="icon-lock"></i></span>
+        <input type="password" id="password" name="password" class="input-xlarge" maxlength="20" placeholder="<?php echo lang('global:password') ?>" />
+    </div>
+    <div class="controls form-inline">
+        <label class="checkbox"><?php echo form_checkbox('remember', '1', false) ?> <?php echo lang('user:remember') ?></label>
+        <button class="btn-u pull-right" type="submit"><?php echo lang('user:login_btn') ?></button>
+    </div>
+    <hr />
+    <h4><?php echo lang('user:reset_password_link');?></h4>
+    <p>no worries, <a class="color-green" href="<?php echo base_url().'users/reset_pass'?>">click here</a> <?php echo lang('user:reset_password_link');?> or <?php echo anchor('register', lang('user:register_btn'));?>.</p>
+    <?php echo form_close() ?>
+</div><!--/row-fluid-->
--- system/cms/modules/users/views/register.php
+++ system/cms/modules/users/views/register.php
@@ -1,48 +1,53 @@
-<h2 class="page-title" id="page_title"><?php echo lang('user:register_header') ?></h2>
-
-<p>
-	<span id="active_step"><?php echo lang('user:register_step1') ?></span> -&gt;
-	<span><?php echo lang('user:register_step2') ?></span>
-</p>
+{{ session:messages success="success-box" notice="notice-box" error="error-box" }}
 
 <?php if ( ! empty($error_string)):?>
-<!-- Woops... -->
-<div class="error-box">
-	<?php echo $error_string;?>
-</div>
+    <div class="error-box">
+        <?php echo $error_string;?>
+    </div>
 <?php endif;?>
 
-<?php echo form_open('register', array('id' => 'register')) ?>
-<ul>
-	
-	<?php if ( ! Settings::get('auto_username')): ?>
-	<li>
-		<label for="username"><?php echo lang('user:username') ?></label>
-		<input type="text" name="username" maxlength="100" value="<?php echo $_user->username ?>" />
-	</li>
-	<?php endif ?>
-	
-	<li>
-		<label for="email"><?php echo lang('global:email') ?></label>
-		<input type="text" name="email" maxlength="100" value="<?php echo $_user->email ?>" />
-		<?php echo form_input('d0ntf1llth1s1n', ' ', 'class="default-form" style="display:none"') ?>
-	</li>
-	
-	<li>
-		<label for="password"><?php echo lang('global:password') ?></label>
-		<input type="password" name="password" maxlength="100" />
-	</li>
 
-	<?php foreach($profile_fields as $field) { if($field['required'] and $field['field_slug'] != 'display_name') { ?>
-	<li>
-		<label for="<?php echo $field['field_slug'] ?>"><?php echo (lang($field['field_name'])) ? lang($field['field_name']) : $field['field_name'];  ?></label>
-		<div class="input"><?php echo $field['input'] ?></div>
-	</li>
-	<?php } } ?>
+<div class="row-fluid margin-bottom-10">
+    <?php echo form_open('register', array('id' => 'register', 'class' => 'reg-page')) ?>
+    <h3><?php echo lang('user:register_header') ?></h3>
+    <p id="step_register">
+        <span id="active_step"><?php echo lang('user:register_step1') ?></span> -&gt;
+        <span><?php echo lang('user:register_step2') ?></span>
+    </p>
 
-	
-	<li>
-		<?php echo form_submit('btnSubmit', lang('user:register_btn')) ?>
-	</li>
-</ul>
-<?php echo form_close() ?>
+    <div class="controls">
+        <?php if ( ! Settings::get('auto_username')): ?>
+            <label for="username"><?php echo lang('user:username') ?> <span class="color-red">*</span></label>
+            <input type="text" name="username" class="span12" maxlength="100" value="<?php echo $_user->username ?>" />
+        <?php endif ?>
+        <label for="email"><?php echo lang('global:email') ?> <span class="color-red">*</span></label>
+        <input type="text" name="email" class="span12" maxlength="100" value="<?php echo $_user->email ?>" />
+        <?php echo form_input('d0ntf1llth1s1n', ' ', 'class="default-form span12" style="display:none"') ?>
+        <?php foreach($profile_fields as $field) { if($field['required'] and $field['field_slug'] != 'display_name') { ?>
+            <label for="<?php echo $field['field_slug'] ?>">
+                <?php echo (lang($field['field_name'])) ? lang($field['field_name']) : $field['field_name'];  ?>
+                <span class="color-red">*</span>
+            </label>
+            <?php echo $field['input'] ?>
+        <?php } } ?>
+    </div>
+
+    <div class="controls">
+        <div class="span6">
+            <label for="password"><?php echo lang('global:password') ?> <span class="color-red">*</span></label>
+            <input type="password" name="password" class="span12" maxlength="100" />
+        </div>
+        <div class="span6">
+            <label>Confirm Password <span class="color-red">*</span></label>
+            <input type="password" name="cpassword" class="span12" maxlength="100" />
+        </div>
+    </div>
+
+    <div class="controls form-inline">
+        <label class="checkbox"><input type="checkbox" name="condition" />&nbsp; I read <a href="">Terms and Conditions</a></label>
+        <button class="btn-u pull-right" type="submit"><?php echo lang('user:register_btn'); ?></button>
+    </div>
+    <hr />
+    <p>Already Signed Up? Click <a href="<?php echo site_url('login');?>" class="color-green">Sign In</a> to login your account.</p>
+    <?php echo form_close(); ?>
+</div><!--/row-fluid-->
