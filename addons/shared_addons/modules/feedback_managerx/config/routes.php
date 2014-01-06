f397618bf99a1a718e983dcc6d001f0aede0a6a3
daolm <daolm89@gmail.com>
2014-01-06 10:06:55 +0700

    gets footer posts

--- addons/shared_addons/modules/feedback_managerx/config/routes.php
+++
@@ -1,5 +0,0 @@
-<?php  defined('BASEPATH') or exit('No direct script access allowed');
-
-// public
-$route['(feedback_manager)/(:num)/(:num)/(:any)']   = 'feedback_manager/view/$4';
-$route['(feedback_manager)/page(/:num)?']           = 'feedback_manager/index$2';
